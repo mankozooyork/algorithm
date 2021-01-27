fe94a7499ad67a1df5a01106598da2999bb8e580
mankozooyork <kozyc93@gmail.com>
2021-01-27 23:28:27 +0900

    add

---
+++ codility/1_CyclicRotation/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        List<Integer> na = new ArrayList<>();
+        int index = 0;
+        for(int i=0; i<A.length; i++) {
+            na.add(A[i]);
+        }
+        for(int i=0; i<K; i++) {
+            Collections.rotate(na,1);
+        }
+        int[] ans = new int[na.size()];
+        for(int i=0; i<na.size(); i++) {
+            ans[i] = na.get(i);
+        }
+        return ans;
+    }
+}
