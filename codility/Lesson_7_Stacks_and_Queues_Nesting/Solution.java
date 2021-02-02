cb515b435de0f5830c6a9115250a181e6337e0b0
mankozooyork <kozyc93@gmail.com>
2021-02-03 01:10:13 +0900

    Nesting

---
+++ codility/Lesson_7_Stacks_and_Queues_Nesting/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(solution("(()(())())"));
+        System.out.println(solution("())"));
+        System.out.println(solution("))(("));
+    }
+    public static int solution(String S) {
+        Stack<Character> s = new Stack<>();
+
+        for(int i=0; i<S.length(); i++) {
+            if(S.charAt(i) == '(') {
+                s.push(S.charAt(i));
+            } else {
+                if(s.isEmpty()) {
+                    return 0;
+                } else {
+                    if(s.peek() == '(') {
+                        s.pop();
+                    }
+                }
+            }
+        }
+        return s.isEmpty() ? 1 : 0;
+    }
+}
