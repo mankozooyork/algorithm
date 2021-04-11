660bdc1b7340ee56debe45a5b61f6294b4d90ad4
IMAI NAMI <imainami@IMAI-no-MacBook-Pro.local>
2021-04-11 23:22:04 +0900

    add

---
+++ AtCoder/ABC198/MainA.java
@@ -0,0 +1,10 @@
+package AtCoder.ABC198;
+import java.util.Scanner;
+public class MainA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println(n-1);
+    }    
+}
---
+++ AtCoder/ABC198/MainB.java
@@ -0,0 +1,8 @@
+package AtCoder.ABC198;
+import java.util.Scanner;
+public class MainB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String
+    }
+}
